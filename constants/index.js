8c754a2ae20bf44a91fd856e22d1be529ee65bb3
StephenChi-hi <nwikechisom77@gmail.com>
2023-07-11 04:15:16 +0100

    Editing the slider

--- constants/index.js
+++ constants/index.js
@@ -2,28 +2,34 @@
 export const dataSlider = [
     {
         id: 1,
-        title: 'Better Devices for Better Life',
-        subTitle: 'Need it , Search it , Find it. Shopping just got better!',
+        title: 'Discover Endless Possibilities',
+        subTitle: 'Explore a world of diverse and customizable products',
+        backgroundImage: 'https://res.cloudinary.com/dcb4ilgmr/image/upload/v1689041858/image_1_1_jaa4fr.png',
+
     },
     {
         id: 2,
-        title: 'Better Devices for Better Life 2',
-        subTitle: 'Need it , Search it , Find it. Shopping just got better! 2',
+        title: 'Customize Your Style',
+        subTitle: 'Make a statement with personalized creations',
+        backgroundImage: 'https://res.cloudinary.com/dcb4ilgmr/image/upload/v1678389895/cld-sample-3.jpg',
     },
     {
         id: 3,
-        title: 'Better Devices for Better Life 3',
-        subTitle: 'Need it , Search it , Find it. Shopping just got better! 3',
+        title: 'Shop with Confidence',
+        subTitle: 'Trustworthy payments, hassle-free returns, and exceptional service.',
+        backgroundImage: 'https://res.cloudinary.com/dcb4ilgmr/image/upload/v1678389894/cld-sample.jpg',
     },
     {
         id: 4,
-        title: 'Better Devices for Better Life 4',
-        subTitle: 'Need it , Search it , Find it. Shopping just got better! 4',
+        title: 'Limited Edition Collections',
+        subTitle: 'Own something truly unique from exclusive collections.',
+		backgroundImage: 'https://res.cloudinary.com/dcb4ilgmr/image/upload/v1678389896/cld-sample-4.jpg',
     },
     {
         id: 5,
-        title: 'Better Devices for Better Life 5',
-        subTitle: 'Need it , Search it , Find it. Shopping just got better! 5',
+        title: 'Join the Community',
+        subTitle: ' Connect with like-minded individuals and unlock exclusive benefits.',
+		backgroundImage: 'https://res.cloudinary.com/dcb4ilgmr/image/upload/v1678389878/samples/imagecon-group.jpg',
     }
 
 ]
